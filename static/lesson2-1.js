e20c761b35e66a044f1d3efe5ba7ba2c7219b7c1
squirrelzt <squirrelzt@126.com>
2020-05-08 21:00:23 +0800

    first commit

--- static/lesson2-1.js
+++ static/lesson2-1.js
@@ -1,1 +1,14 @@
-var abc = 1234
+var obj = {b:222};
+obj.a = 111;
+console.log(obj);
+console.log(obj.a)
+for(var i =0; i < 3; i++) {
+    setTimeout(function(){
+        console.log(i)
+    },1000)
+}
+for(let j =0; j < 3; j++) {
+    setTimeout(function(){
+        console.log(j)
+    },1000)
+}
